8d3dd9b0151e9483e0b7cd27d1a3d3e323739857
Jhancn <jhancn39@gmail.com>
2023-11-06 13:08:37 +0530

    variable port

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 app.get('/', (req,res) => {
     res.send("Welcome to the home page");
 });
-app.listen(3000, ()=>{
+const port = process.env.port ||3000;
+app.listen(port, ()=>{
     console.log('listening');
 });
